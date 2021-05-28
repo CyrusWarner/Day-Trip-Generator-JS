792561b52fb8a9b527a2e8601883c7c9a77530de
CyrusWarner <cyruswarner27@gmail.com>
2021-05-28 14:35:07 -0500

    Added if else statement to function entertainment,Transportation, restaurant and destination

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,21 +1,27 @@
+let finalDestination;
+let finalRestaurant;
+let finalTransportationMethod;
+let finalEntertainment;
+
+
 let destination = ["Milwaukee", "Oshkosh", "Atlanta", "Chicago"]
 
 function randomDestination(anArrayOfDestinations){
    let tripDestination = anArrayOfDestinations[Math.floor(Math.random() * anArrayOfDestinations.length)];
     console.log(tripDestination);
 
-    let reSelectDestination = prompt("Would you like a new random destination?");
-if(reSelectDestination == "yes"){
-   randomDestination(destination);
-}
-else{
-    return;
-}
+    let reSelectDestination = prompt("Would you like a new random destination? if so type yes!");
+    if(reSelectDestination == "yes"){
+    return randomDestination(destination);
+    }
+    else{
+        return tripDestination;
+    }
  
 
 }
 
-let finalDestination = randomDestination(destination);
+finalDestination = randomDestination(destination);
 
 
 
@@ -25,29 +31,35 @@ function randomRestaurant(anArrayOfRestaurants){
     let tripRestaurant = anArrayOfRestaurants[Math.floor(Math.random () * anArrayOfRestaurants.length)];
     console.log(tripRestaurant);
 
-    let reSelectRestaurant = prompt("Would you like a new random restaurant");
-if(reSelectRestaurant == "yes"){
-        randomRestaurant(restaurant);
+    let reSelectRestaurant = prompt("Would you like a new random restaurant? if so type yes");
+    if(reSelectRestaurant == "yes"){
+    return randomRestaurant(restaurant);
     }
-else{
-        return;
+    else{
+        return tripRestaurant;
 }
 
 
 }
 
     
-
 let finalRestaurant = randomRestaurant(restaurant);
  
 
 
-let modeOfTransportation = ["Car", "Train", "Airplane"];
+let modeOfTransportation = ["Car", "Bus", "Train", "Airplane"];
 
 function randomModeOfTransportation(anArrayOfMethodsOfTransportation){
     let tripTransitionMethod = anArrayOfMethodsOfTransportation[Math.floor(Math.random() * anArrayOfMethodsOfTransportation.length)];
     console.log(tripTransitionMethod);
-    return tripTransitionMethod;
+    
+    let reSelectTransportation = prompt("Would you like a new method of transportation? if so type yes")
+    if(reSelectTransportation == "yes"){
+        return randomModeOfTransportation(modeOfTransportation);
+    }
+    else{
+        return tripTransitionMethod;
+    }
 
 }
 
@@ -60,7 +72,15 @@ let entertainment = ["Movie", "Sports games", "Comedy performance"];
 function randomFormOfEntertainment(anArrayOfFormsOfEntertainment){
     let TripEntertainment = anArrayOfFormsOfEntertainment[Math.floor(Math.random() * anArrayOfFormsOfEntertainment.length)];
     console.log(TripEntertainment);
-    return TripEntertainment;
+
+    let reSelectAFormOfEntertainment = prompt("Would you like a new form of entertainment? if so type yes");
+    if(reSelectAFormOfEntertainment == "yes"){
+        return randomFormOfEntertainment(entertainment);
+    }
+    else{
+        return TripEntertainment;
+    }
+    
 
 }
 
