122486b8a5f5466b948f1b951873592721544f63
CyrusWarner <cyruswarner27@gmail.com>
2021-05-28 11:33:20 -0500

    Function added for random Restaurant

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -3,8 +3,20 @@ let destination = ["Milwaukee", "Oshkosh", "Atlanta", "Chicago"]
 function randomDestination(anArrayOfDestinations){
    let tripDestination = anArrayOfDestinations[Math.floor(Math.random() * anArrayOfDestinations.length)];
     console.log(tripDestination);
+    return tripDestination;
 
 
 }
 
 let finalDestination = randomDestination(destination);
+
+let restaurant = ["Mcdonalds", "Chick-fill-A", "Arbys", "Chipotle"];
+
+function randomRestaurant(anArrayOfRestaurants){
+    let tripRestaurant = anArrayOfRestaurants[Math.floor(Math.random () * anArrayOfRestaurants.length)];
+    console.log(tripRestaurant);
+    return tripRestaurant;
+
+}
+
+let finalRestaurant = randomRestaurant(restaurant);
